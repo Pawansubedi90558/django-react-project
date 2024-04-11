9a7b7978f83eda30f20a898915c3a798531e8692
Pawan Subedi <pawansubedi90558@gmail.com>
2024-04-11 07:04:20 +0545

    model created for notes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Note(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    #on_delete=models.CASCADE means that if the user is deleted, all notes associated with that user will also be deleted
+    #related_name="notes" is used to access all notes associated with a user
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name="notes")
+
+    def __str__(self):
+        return self.title
