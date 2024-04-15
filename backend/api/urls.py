d902ae1f15c108e15c93b6a16f2bd55c2706ffb6
Pawan Subedi <pawansubedi90558@gmail.com>
2024-04-15 14:31:51 +0545

    urls re-configured

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path("notes/",views.NoteListCreate.as_view(),name="note-list").
-    path("notes/delete/<int:pk>/",views.NoteDelete.as_view(),name="delete-note"),
+    path("notes/",views.NoteListCreate.as_view(),name="note-list"),
+    path("notes/delete/<int:pk>/",views.NoteDelete.as_view(),name="delete-note")
 ]
