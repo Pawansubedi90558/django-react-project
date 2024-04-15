7699a9d4f215c9b8cd277861da1989aa5dccf6ac
Pawan Subedi <pawansubedi90558@gmail.com>
2024-04-15 13:52:34 +0545

    function for getting the notes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated,AllowAny
+from .models import Note
 
 # Create your views here.
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class=NoteSerializer
+    permission_classes=[IsAuthenticated]
+
+    def get_queryset(self):
+        user=self.request.user
+        return Note.objects.filter(author=user)
+    
 class CreateUserView(generics.CreateAPIView):
     queryset=User.objects.all()
-    serializer_class=UserSerializer
+    serializer_class=UserSerializer 
     permission_classes=[AllowAny]
