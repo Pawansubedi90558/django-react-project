2cea5ce85d7788acae752d647b581ae0fe91a250
Pawan Subedi <pawansubedi90558@gmail.com>
2024-04-24 00:03:30 +0545

    routes configured for all pages; not found page too

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import react from "react"
 import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom"
+import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import NotFound from "./pages/NotFound"
@@ -16,9 +17,23 @@ function RegisterAndLogout(){
 }
 function App() {
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<RegisterAndLogout />}/>
+        <Route path="*" element={<NotFound />} />
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
