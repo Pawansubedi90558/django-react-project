a288d3f6dcdc03fe3b6bc2f56f7b6b1c67fe48e5
Pawan Subedi <pawansubedi90558@gmail.com>
2024-04-24 00:18:48 +0545

    not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,4 +1,7 @@
 function NotFound(){
-    return <div>NotFound</div>
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>The page you are looking for doesn't exist.</p>
+    </div>
 }
 export default NotFound
