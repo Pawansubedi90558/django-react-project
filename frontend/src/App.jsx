79ea0008bcdf30c3df045e659d26afc3fdf63c92
Pawan Subedi <pawansubedi90558@gmail.com>
2024-04-23 23:37:46 +0545

    logout and register with clearing locally stored keys

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
 import react from "react"
-import {BrowserRouter,Routes,Route,Navigation}
+import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout(){
+  localStorage.clear()
+  return <Navigate to="/Login"/>
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
 function App() {
   return (
     <>
