09c575f504065838a73906b4e411c8573f81edb0
Pawan Subedi <pawansubedi90558@gmail.com>
2024-04-23 23:14:59 +0545

    token authorization done

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,16 +3,44 @@ import {Navigate} from "react-router-dom"
 import {jwtDecode} from "jwt-decode"
 import api from "../api"
 import { REFRESH_TOKEN,ACCESS_TOKEN } from "../constants"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 function ProtectedRoute({children}){
     const [isAuthorized,setIsAuthorized] = useState(null)
 
+    useEffect(() => {
+        auth().catch(() =>setIsAuthorized(false))
+    },[])
+    
     const refreshToken = async() =>{
-
+        try {
+            const res = await api.post("/api/token/refresh/",{refresh: refreshToken});
+            if(res.status == 200){
+                localStorage.setItem(ACCESS_TOKEN,res.data.access)
+                setIsAuthorized(true)
+            }else{
+                setIsAuthorized(false)
+            }
+        } catch (error) {
+            console.log(error)
+            setIsAuthorized(false)
+        }
     }
     const auth = async()=>{
+        const token = localStorage.getItem(ACCESS_TOKEN)
+        if(!token){
+            setIsAuthorized(false)
+            return 
+        }
+        const decoded = jwtDecode(token)
+        const tokenExpiration = decoded.exp;
+        const now = Date.now()/1000
 
+        if(tokenExpiration < now){
+            await refreshToken()
+        }else{
+            setIsAuthorized(true)
+        }
     }
     if(isAuthorized === null){
         return <div>Loading...</div>
