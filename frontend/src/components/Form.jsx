b62af0bad704e2ee0e5a415ad0faf571edc5ff43
Pawan Subedi <pawansubedi90558@gmail.com>
2024-04-24 17:08:18 +0545

    form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -10,6 +10,26 @@ function Form({form, route}){
     const navigate =useNavigate()
 
     const choice = method === "login"?"Login":"Register"
+
+    const handleSubmit =async (e) =>{
+        setLoading(true);
+        e.preventDefault();
+
+        try{
+            const res= await api.post(route,{username,password})
+            if(method === "login"){
+                localStorage.setItem(ACCESS_TOKEN,res.data.access);
+                localStorage.setItem(REFRESH_TOKEN,res.data.refresh);
+                navigate("/")
+            }else{
+                navigate("/login")
+            }
+        }catch(error){
+            alert(error)
+        }finally{
+            setLoading(false)
+        }
+    };
     return <form onSubmit={handleSubmit} className="form-container">
         <h1>{choice}</h1>
         <input 
